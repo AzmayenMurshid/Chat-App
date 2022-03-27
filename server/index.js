9981c44b93b2ed61b9a6d061418af1a4eb77b4c7
Azmayen Murshid <azmayen.murshid@gmail.com>
2022-03-27 06:59:30 +0000

    Backend Auth

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const cors = require(cors);
+const express= require('express');
+const cors = require('cors');
+
+const authRoutes = require("./routes/auth.js");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT=process.env.PORT || 5000;
 
 require('dotenv').config();
 
@@ -11,7 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World!');
 });
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.use('/auth', authRoutes)
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
