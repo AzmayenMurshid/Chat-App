5b9c91c4f8b391c696b8e1fd46d879831ecf7d7f
Azmayen Murshid <azmayen.murshid@gmail.com>
2022-03-27 07:23:49 +0000

    Client-Server Communication.

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 
 import signinImage from '../assets/signup.jpg'
 
+const cookies = new Cookies();
+
 const initialState = {
     fullName: '',
     username: '',
@@ -21,10 +23,29 @@ const Auth = () => {
         setForm({...form, [e.target.name]: e.target.value});
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async(e) => {
         e.preventDefault();
 
-        console.log(form);
+        const { fullName, username, password, phoneNUmber, avatarURL } = form;
+
+        const URL = 'http://localhost:5000/auth';
+
+        const { data: {token, userID, hashedPassword} } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+            username, password, fullName, phoneNumber, avatarURL,
+        }
+        )
+        cookies.set('username', username)
+        cookies.set('fullName', fullName)
+        cookies.set('userId', userId)
+
+        if(isSignup) {
+            cookies.set('phoneNumber', phoneNumber)
+            cookies.set('avatarURL', avatarURL)
+            cookies.set('hashedPassword', hashedPassword)
+        }
+
+        window.location.reload();
+
     }
 
     const switchMode = () => {
